39cbd1ad0b87f447b3f4dde8f77a30e967afd5ef
drinkingkazu <kazuhiro@nevis.columbia.edu>
2018-11-22 08:56:52 +0000

    cosmetics

--- iotools/io_base.py
+++ iotools/io_base.py
@@ -4,7 +4,13 @@ import time
 class io_base(object):
 
     def __init__(self, flags):
-        self._batch_size   = flags.BATCH_SIZE
+
+        if flags.BATCH_SIZE % len(flags.GPUS):
+            print('BATCH_SIZE (%d) must be divisible by GPU count (%d)' % (flags.BATCH_SIZE, len(flags.GPUS)))
+            raise ValueError
+        
+        self._batch_size    = flags.BATCH_SIZE
+        self._batch_per_gpu = flags.BATCH_SIZE / len(flags.GPUS)
         self._num_entries  = -1
         self._num_channels = -1
         self._flags = flags
@@ -15,9 +21,11 @@ class io_base(object):
     def blob(self):
         return self._blob
 
-    def batch_size(self, size=None):
-        if size is None: return self._batch_size
-        self._batch_size = int(size)
+    def batch_size(self):
+        return self._batch_size
+
+    def batch_per_gpu(self):
+        return self._batch_per_gpu
 
     def num_entries(self):
         return self._num_entries
--- iotools/threadio_func.py
+++ iotools/threadio_func.py
@@ -13,7 +13,7 @@ def threadio_func(io_handle, thread_id):
     """
     num_gpus = len(io_handle._flags.GPUS)
     batch_size = io_handle.batch_size()
-    batch_per_gpu = batch_size / num_gpus
+    batch_per_gpu = io_handle.batch_per_gpu()
     while 1:
         time.sleep(0.000001)
         while not io_handle._locks[thread_id]:
@@ -23,24 +23,24 @@ def threadio_func(io_handle, thread_id):
             new_idx_v = []
             # label_v   = []
             blob = {}
-            for key, val in io_handle._blob.iteritems():
+            for key, val in io_handle.blob().iteritems():
                 blob[key] = []
             if io_handle._flags.SHUFFLE:
                 idx_v = np.random.random([batch_size])*io_handle.num_entries()
                 idx_v = idx_v.astype(np.int32)
-                # for key, val in io_handle._blob.iteritems():
+                # for key, val in io_handle.blob().iteritems():
                 #     blob[key] = val  # fixme start, val?
             else:
                 start = io_handle._start_idx[thread_id]
                 end   = start + batch_size
                 if end < io_handle.num_entries():
                     idx_v = np.arange(start,end)
-                    # for key, val in io_handle._blob.iteritems():
+                    # for key, val in io_handle.blob().iteritems():
                     #     blob[key] = val[start:end]
                 else:
                     idx_v = np.arange(start, io_handle.num_entries())
                     idx_v = np.concatenate([idx_v,np.arange(0,end-io_handle.num_entries())])
-                    # for key, val in io_handle._blob.iteritems():
+                    # for key, val in io_handle.blob().iteritems():
                     #     blob[key] = val[start:] + val[0:end-io_handle.num_entries()]
                 next_start = start + len(io_handle._threads) * batch_size
                 if next_start >= io_handle.num_entries():
@@ -55,16 +55,16 @@ def threadio_func(io_handle, thread_id):
                     blob[key].append([])
 
             for data_id, idx in enumerate(idx_v):
-                voxel   = io_handle._blob['voxels'][idx]
+                voxel  = io_handle.blob()['voxels'][idx]
                 new_id = data_id / batch_per_gpu
                 voxel_v[new_id].append(np.pad(voxel, [(0,0),(0,1)],'constant',constant_values=data_id))
-                feature_v[new_id].append(io_handle._blob['feature'][idx])
+                feature_v[new_id].append(io_handle.blob()['feature'][idx])
                 new_idx_v[new_id].append(idx)
                 for key in io_handle._flags.DATA_KEYS:
-                    blob[key][new_id].append(io_handle._blob[key][idx])
+                    blob[key][new_id].append(io_handle.blob()[key][idx])
                 # if len(io_handle._label):
                 #     label_v.append(io_handle._label[idx])
-            blob['voxels']   = [np.vstack(voxel_v[i]) for i in range(num_gpus)]
+            blob['voxels']  = [np.vstack(voxel_v[i]) for i in range(num_gpus)]
             blob['feature'] = [np.vstack(feature_v[i]) for i in range(num_gpus)]
             new_idx_v = [np.array(x) for x in new_idx_v]
             # if len(label_v): label_v = np.hstack(label_v)
